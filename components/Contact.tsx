acc298eae23d7578a4316d9f2b1598458aeef6ef
Luminous <84202428+Rohit-Pujari@users.noreply.github.com>
2026-08-12 20:00:54 +0530

    Resume link update
    Updated the resume link to point to the new and updated resume

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -72,7 +72,7 @@ function Contact() {
               Send email
             </a>
             <a
-              href="https://drive.google.com/file/d/1Sd9jTVKypF59hzVjP4cbsUV9yxEIqH7I/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1v02TT4bbcscW-gDqd2rXxu_S2m0PLPWt/"
               target="_blank"
               rel="noopener noreferrer"
               className="rounded-full border border-white/20 bg-white/5 px-5 py-2.5 text-sm font-semibold text-white transition-colors hover:bg-white/10"
